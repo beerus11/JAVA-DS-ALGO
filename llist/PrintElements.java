7eae0fb340ac0b5ba4985c3d857a1b5f70eae054
Anurag Goel <thechampanurag@gmail.com>
2016-05-03 18:52:10 +0530

    PrintElements.java

---
+++ llist/PrintElements.java
@@ -0,0 +1,29 @@
+class PrintElements {
+ static class Node {
+  int data;
+  Node next;
+ }
+ public static void main(String args[]) {
+  Node node1 = new Node();
+  node1.data = 1;
+  Node node2 = new Node();
+  node2.data = 2;
+  Node node3 = new Node();
+  node3.data = 3;
+  node1.next = node2;
+  node2.next = node3;
+  node3.next = null;
+  printList(node1);
+
+ }
+
+ public static void printList(Node node) {
+  Node currentNode = node;
+  while (currentNode != null) {
+   System.out.println(currentNode.data);
+   currentNode = currentNode.next;
+  }
+
+
+ }
+}
