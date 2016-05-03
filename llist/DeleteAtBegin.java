5305de2723409e05be7f44e0a1b28df1404adcd9
Anurag Goel <thechampanurag@gmail.com>
2016-05-03 19:02:07 +0530

    DeleteAtBegin.java

---
+++ llist/DeleteAtBegin.java
@@ -0,0 +1,37 @@
+class DeleteAtBegin {
+ static class Node {
+  int data;
+  Node next;
+ }
+ public static void main(String args[]) {
+ Node node1 = new Node();
+  node1.data = 1;
+  Node node2 = new Node();
+  node2.data = 2;
+  Node node3 = new Node();
+  node3.data = 3;
+  node1.next = node2;
+  node2.next = node3;
+  node3.next = null;
+  printList(delete(node1)); 
+ }
+
+ public static Node delete(Node head) {
+  Node temp= head;
+  Node currentNode= temp.next;
+  temp=null;
+  return currentNode;
+ 
+ }
+
+
+ public static void printList(Node node) {
+  Node currentNode = node;
+  while (currentNode != null) {
+   System.out.println(currentNode.data);
+   currentNode = currentNode.next;
+  }
+
+
+ }
+}
