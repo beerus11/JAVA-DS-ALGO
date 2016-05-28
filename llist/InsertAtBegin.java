68a3fb2d5baa69a31144c9c4e9306d7876caaa0f
Anurag Goel <thechampanurag@gmail.com>
2016-05-28 11:28:56 +0530

    InsertAtbegin

--- llist/InsertAtBegin.java
+++ llist/InsertAtBegin.java
@@ -1,14 +1,28 @@
+/**
+ *@author     Anurag Goel
+ * Example : Insert At Beginning of Linked List
+ */
 class InsertAtBegin {
  static class Node {
   int data;
   Node next;
  }
+ /**
+  * Main Method
+  */
  public static void main(String args[]) {
   Node node = insert(insert(insert(null, 1), 2), 3);
   printList(node);
 
  }
-
+ /**
+  * Insert Node At Beginning of Linked List
+  *
+  * @param      head  The head
+  * @param      data  The data
+  *
+  * @return     return head of Linked List
+  */
  public static Node insert(Node head, int data) {
   if (head == null) {
    Node node = new Node();
@@ -20,10 +34,13 @@ class InsertAtBegin {
   node.data = data;
   node.next = head;
   return node;
-
  }
 
-
+/**
+ * Print All Elements of Linked List
+ *
+ * @param      Head of Linked List
+ */
  public static void printList(Node node) {
   Node currentNode = node;
   while (currentNode != null) {
