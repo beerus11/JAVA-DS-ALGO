8f4d2f303c087f20b5ea0d1c448309522f089ff3
Anurag Goel <thechampanurag@gmail.com>
2016-07-12 16:18:08 +0530

    QuickSort

--- sorting/QuickSort.java
+++ sorting/QuickSort.java
@@ -1,7 +1,43 @@
-package sorting;
-
 public class QuickSort {
 	public static void main(String args[]) {
+		int array[]={9,2,5,1,7,1,2,9,5,4};
+		quickSort(array,0,array.length-1);
+		for(int e : array)
+		{
+			System.out.println(""+e);
+		}
 	}
+
+	static void quickSort(int [] arr , int left ,int right)
+	{
+		if(left<right)
+		{
+		int p = partition(arr,left,right);
+		quickSort(arr,left,p-1);
+		quickSort(arr,p+1,right);
+		}
+	}
+
+static int partition(int [] arr,int l, int h)
+{
+	int pivot=arr[h];
+	int i=l-1;
+	for(int j=l;j<h;j++)
+	{
+		if(arr[j]<=pivot)
+		{
+			swap(arr,i+1,j);
+			i++;
+		}
+	}
+swap(arr,i+1,h);
+return i+1;
+}
+static void swap(int arr[] ,int i , int j)
+{
+	int temp= arr[i];
+	arr[i]=arr[j];
+	arr[j]=temp;
+}
 
 }
